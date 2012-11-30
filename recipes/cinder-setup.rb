4ae8ace10fff0221270c79d22d5030d86a765a60
Darren Birkett <darren.birkett@gmail.com>
2012-11-30 15:27:19 +0000

    use api not volume for service namespace

--- recipes/cinder-setup.rb
+++ recipes/cinder-setup.rb
@@ -41,7 +41,7 @@ keystone_admin_user = keystone["admin_user"]
 keystone_admin_password = keystone["users"][keystone_admin_user]["password"]
 keystone_admin_tenant = keystone["users"][keystone_admin_user]["default_tenant"]
 
-volume_endpoint = get_access_endpoint("cinder-api", "cinder", "volume")
+volume_endpoint = get_access_endpoint("cinder-api", "cinder", "api")
 if volume_endpoint.nil?
     Chef::Log.info("Rolling back to search for nova-volume")
     volume_endpoint = get_access_endpoint("nova-volume", "nova", "volume")
