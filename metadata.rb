dd9bed49b6a5b5776986dd0a4cb9917ea56856b5
Joe Breu <breu@breu.org>
2013-10-08 18:00:23 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Rackspace US, Inc."
 license          "Apache 2.0"
 description      "Installs/Configures cinder"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.10"
+version           "4.2.0"
 recipe           "cinder-api", ""
 recipe           "cinder-scheduler", ""
 recipe           "cinder-volume", ""
