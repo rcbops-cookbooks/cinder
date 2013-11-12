830b870a3a26ecbdc7843c99d4ddf9d35115945b
Darren Birkett <darren.birkett@gmail.com>
2013-11-12 12:14:03 +0000

    add full rbd configuration to cinder-volume recipe

--- recipes/cinder-volume.rb
+++ recipes/cinder-volume.rb
@@ -106,6 +106,7 @@ case node["cinder"]["storage"]["provider"]
       )
       notifies :restart, "service[cinder-volume]", :delayed
     end
+
   when "lvm"
     template node["cinder"]["storage"]["lvm"]["config"] do
       source "lvm.conf.erb"
@@ -113,5 +114,60 @@ case node["cinder"]["storage"]["provider"]
       variables(
         "volume_group" => node["cinder"]["storage"]["lvm"]["volume_group"]
       )
+    end
+
+  when "rbd"
+
+    if node['ceph']['install_method'] == "chef"
+
+      include_recipe "ceph::repo"
+      include_recipe "ceph"
+      include_recipe "ceph::conf"
+
+      rbd_user = node['cinder']['storage']['rbd']['rbd_user']
+      rbd_pool = node['cinder']['storage']['rbd']['rbd_pool']
+      rbd_pool_pg_num = node['cinder']['storage']['rbd']['rbd_pool_pg_num']
+      rbd_secret_uuid = node['cinder']['storage']['rbd']['rbd_secret_uuid']
+
+      # do all this in a ruby_block so it doesn't get executed at compile time
+      ruby_block "create cinder cephx client" do
+        block do
+
+          admin_client_keyring_file="/etc/ceph/ceph.client.admin.keyring"
+          mon_keyring_file = "#{Chef::Config[:file_cache_path]}/#{node['hostname']}.mon.keyring"
+
+          # create the admin client keyring
+          unless File.exist?(admin_client_keyring_file)
+
+            monitor_secret = if node['ceph']['encrypted_data_bags']
+                               secret = Chef::EncryptedDataBagItem.load_secret(node["ceph"]["mon"]["secret_file"])
+                               Chef::EncryptedDataBagItem.load("ceph", "mon", secret)["secret"]
+                             else
+                               node["ceph"]["monitor-secret"]
+                             end
+
+            # create the mon keyring temporarily
+            Mixlib::ShellOut.new("ceph-authtool '#{mon_keyring_file}' --create-keyring --name='mon.' --add-key='#{monitor_secret}' --cap mon 'allow *'").run_command
+
+            # write out the client admin keyring
+            admin_client_keyring = Mixlib::ShellOut.new("ceph auth get-or-create client.admin --name='mon.' --keyring='#{mon_keyring_file}'").run_command.stdout
+            f = File.open(admin_client_keyring_file, 'w')
+            f.write(admin_client_keyring)
+            f.close
+
+            # remove the temporary mon keyring
+            File.delete(mon_keyring_file)
+          end
+
+          # Ensure the rbd user exists and has appropriate pool permissions).
+          # TODO(mancdaz): get glance pool name by search, and only grant access if glance is using rbd
+          Mixlib::ShellOut.new("ceph auth get-or-create client.#{rbd_user}").run_command
+          Mixlib::ShellOut.new("ceph auth caps client.#{rbd_user} mon 'allow r' osd 'allow class-read object_prefix rbd_children, allow rwx pool=#{rbd_pool}, allow rx pool=images'").run_command
+
+          # get the key for this user and set it to the node hash so it's
+          # searchable as nova::libvirt needs it
+          #
+        end
+      end
     end
 end
