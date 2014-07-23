bc74f5875bd5b61b072f9b46b494560df63018b0
Duncan Thomas <duncan.thomas@gmail.com>
2014-07-23 19:13:53 +0100

    Update modwsgi script to match recent cinder changes

--- files/default/cinder_modwsgi.py
+++ files/default/cinder_modwsgi.py
@@ -1,10 +1,21 @@
-import sys
-from cinder.openstack.common import log as logging
 from paste import deploy
-from cinder import flags
 
-flags.parse_args(sys.argv)
+from oslo.config import cfg
+
+from cinder.common import config
+from cinder.openstack.common import gettextutils
+from cinder.openstack.common import log as logging
+from cinder import rpc
+from cinder import version
+
+gettextutils.enable_lazy()
+
+CONF=cfg.CONF
+CONF("", project='cinder', version=version.version_string())
+
 logging.setup("cinder")
+
+rpc.init(CONF)
 
 conf = '/etc/cinder/api-paste.ini'
 name = "osapi_volume"
